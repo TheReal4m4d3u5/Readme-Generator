0ef778bbd65721b0144f1269beacac0bdc6f8a2b
Avery Jacobson <averyajacobson@gmail.com>
2024-10-11 14:41:34 -0500

    added my renderLicenseSection function

--- utils/generateMarkedown.js
+++ utils/generateMarkedown.js
@@ -33,3 +33,18 @@ function renderLicenseLink(license) {
     }
   
   }
+
+
+  // TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+
+    let licenseMessage = "This application is covered under the following license: "
+  
+    if(license !== ''){
+      return licenseMessage 
+    }else{
+      return "";
+    }
+    
+  }
