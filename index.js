cd2d07cc9ec7edd35fab0f4877df5faa2d581e64
Avery Jacobson <averyajacobson@gmail.com>
2024-10-11 14:38:07 -0500

    added my writeFile function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import inquirer from 'inquirer';
 import fs from 'fs';
 import markdown from './utils/generateMarkdown.js';
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.info("Written to file.")
+    });
+}
